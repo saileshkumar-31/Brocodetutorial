ddf1116df3a5fee113b339a73e093524b4a13af9
saileshkumar-31 <saileshkumar@Saileshs-Macbook.local>
2025-10-21 18:14:31 +0530

    calculate area code added

--- src/$4calculatetheareaoftriangle.java
+++ src/$4calculatetheareaoftriangle.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
 public class $4calculatetheareaoftriangle {
+
+    public static void main(String[] args) {
+        //calculate the area of triangle
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the width: ");
+        width = scanner.nextDouble();
+
+        System.out.print("Enter the height: ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+        System.out.println("The area is " + area);
+
+        scanner.close();
+
+    }
 }
